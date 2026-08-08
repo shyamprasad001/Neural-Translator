b78b2d27bc7c9ec49112f1fb4e1fa9727c68b4e6
Shyam Mantri <iamshyam1911@gmail.com>
2026-08-08 13:15:00 +0530

    Create dataset module

---
+++ dataset/dataset.py
@@ -0,0 +1,11 @@
+# dataset.py
+
+# A very simple, hardcoded dataset for our mini-project.
+# This ensures training is extremely fast.
+
+data = [
+    ("I am happy", "Je suis heureux"),
+    ("I am tired", "Je suis fatigué"),
+    ("I am a student", "Je suis étudiant"),
+    ("I am a teacher", "Je suis professeur")
+]
