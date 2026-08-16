abe9f9880a27f4922f606792ef52c74966283ad0
Shyam Mantri <iamshyam1911@gmail.com>
2026-08-16 17:55:04 +0530

    Bind Flask to 0.0.0.0 and PORT for Render deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 import translator
 
@@ -41,4 +42,5 @@ def translate_sentence():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
